a3cfade295725ee698041d594a7d332ccd0f06d1
ST10468548 <ST10468548@rcconnect.edu.za>
2025-06-19 12:56:26 +0200

    ratings and return button
    the full ratings are 28 for all four songs

--- app/src/main/java/com/example/trendy/MainActivity.kt
+++ app/src/main/java/com/example/trendy/MainActivity.kt
@@ -34,14 +34,19 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Trendy" +
+                "main screen first screen" +
                 "val 1 what if rap song by yanga" +
                 "val 2 all of the lights rnb song by Kanye West" +
                 "val 3 hey hey gqom by Shado M" +
                 "val 4 bow down Gospel song by Benjamin Dube" +
+                "second screen first button" +
                 "Rating what if 8" +
                 "all of the lights 8" +
                 "hey hey 4" +
-                "bow down 8",
+                "bow down 8" +
+                "Exit button" +
+                "return button" +
+                "ratings are for all four songs = 28",
         modifier = modifier
     )
 }
