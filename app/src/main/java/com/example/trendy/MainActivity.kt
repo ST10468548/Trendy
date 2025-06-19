9d982bf902e2baa8ec24e87db59c2a239c314c56
ST10468548 <ST10468548@rcconnect.edu.za>
2025-06-19 12:33:52 +0200

    Added a new name : Trendy

--- app/src/main/java/com/example/trendy/MainActivity.kt
+++ app/src/main/java/com/example/trendy/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             TrendyTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Trendy",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
